5700d8be574f34ac42cdf84743b91a33344ed7dc
iago-casa-Ubuntu <soloiago@gmail.com>
2012-05-09 23:28:47 +0200

    first commit

---
+++ soloiago.py
@@ -0,0 +1,35 @@
+import webapp2
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+
+class WebHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainPage(WebHandler):
+  def get(self):
+    self.render("header.txt")
+    self.render("index.html")
+    self.render("foot.txt")
+
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage)
+], debug=True)
